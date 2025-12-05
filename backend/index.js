f0b621fdbb564c1258d525e1cada5d6dceec9363
Shadab khan <techshad04@gmail.com>
2025-12-05 15:01:14 +0530

    sssss

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3036",
-  "https://www.collegeforms.in",
+  "https://collegeforms.in",
   "https://www.collegeforms.in",
   "https://www.collegeforms.in",
 
