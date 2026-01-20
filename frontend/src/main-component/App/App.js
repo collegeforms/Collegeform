b72f2fbbca3238f8dfd4b26132f2cbc20a2cb978
Gajendra patidar <155809773+Gajendra-patidar@users.noreply.github.com>
2026-01-20 16:14:05 +0530

    Add FloatingButtons component

--- frontend/src/main-component/App/App.js
+++ frontend/src/main-component/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AllRoute from '../router'
 // import {ToastContainer} from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
+import FloatingButtons from "../../components/FloatingButtons";
 
 
 const App = () => { 
 
   return (
     <div className="App" id='scrool'>
+    <FloatingButtons />
           <AllRoute/>
           {/* <ToastContainer/> */}
     </div>
